4c3b9942c4b568dbe53b3a7eb73fa608b0d8e45f
Rajab Mkomwa <rajeyw@gmail.com>
2019-03-30 13:46:04 +0300

    feat: add test artifact and support to run es6 when in node environment

--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -44,12 +44,7 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: 'babel-loader'
       },
       {
         test: /\.js?$/,
